26043fe9ffc2dd05edf58e9226e1bbb2cfc2f166
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-10-30 08:47:36 +0100

    Configure biome.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [],
-	base: "/02-website-project/",
-	build: {
-		outDir: "dist",
-	},
+  plugins: [],
+  base: "/02-website-project/",
+  build: {
+    outDir: "dist",
+  },
 });
