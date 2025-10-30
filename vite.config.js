0a2dc47d05b98d5b0f1cbf31f8d3048fa4c7aeae
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-10-30 08:32:08 +0100

    Add linter.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [],
-  base: '/02-website-project/',
-  build: {
-    outDir: 'dist'
-  },
-})
+	plugins: [],
+	base: "/02-website-project/",
+	build: {
+		outDir: "dist",
+	},
+});
