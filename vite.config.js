1dd5eed36ba1cf2f72fae6a7f9f0350ab333c44c
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-10-30 09:45:39 +0100

    Add subsite.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,23 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import fs from "fs";
+
+function getHtmlInputs() {
+  const inputs = {};
+  // Hauptverzeichnis index.html
+  inputs.main = resolve(__dirname, "index.html");
+  const dir = resolve(__dirname, "unterseiten");
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    if (file.endsWith(".html")) {
+      const name = file.replace(".html", "");
+      inputs[name] = resolve(dir, file);
+    }
+  });
+
+  return inputs;
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -6,5 +25,8 @@ export default defineConfig({
   base: "/02-website-project/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: getHtmlInputs(),
+    },
   },
 });
